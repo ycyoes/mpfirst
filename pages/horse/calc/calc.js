421307341c3da88cdeeadfcbb174b010cbec2b61
ycyoes <ycyoes@163.com>
2020-04-23 22:41:50 +0800

    播放器

--- pages/horse/calc/calc.js
+++ pages/horse/calc/calc.js
@@ -1,3 +1,17 @@
 Page({
-  
+  data: {
+    item: 0,
+    tab: 0
+  },
+
+  changeItem: function(e) {
+    this.setData({
+      item: e.target.dataset.item
+    })
+  },
+  changeTab: function(e) {
+    this.setData({
+      tab: e.detail.current
+    })
+  }
 })
