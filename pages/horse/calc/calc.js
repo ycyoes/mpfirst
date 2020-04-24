a885ceb79469dfc2478a934ff3bf94eb6702b561
ycyoes <ycyoes@163.com>
2020-04-24 21:02:25 +0800

    音乐播放器

--- pages/horse/calc/calc.js
+++ pages/horse/calc/calc.js
@@ -1,7 +1,50 @@
 Page({
   data: {
     item: 0,
-    tab: 0
+    tab: 0,
+    playlist: [{
+      id: 1, title: '钢琴协奏曲', singer: '肖邦',
+      src: '', coverImgUrl:'../../images/cover.jpg'
+    },{
+      id: 2, title: '奏鸣曲', singer: '莫扎特',
+      src: '', coverImgUrl:'../../images/cover.jpg'
+    },{
+      id: 3, title: '欢乐颂', singer: '贝多芬',
+      src: '', coverImgUrl:'../../images/cover.jpg'
+    },{
+      id: 4, title: '爱之梦', singer: '李斯特',
+      src: '', coverImgUrl:'../../images/cover.jpg'
+    }],
+    state: 'paused',
+    playIndex: 0,
+    play: {
+      currentTime: '00:00',
+      duration:'00:00',
+      percent: 0,
+      title: '',
+      singer: '',
+      coverImgUrl: '../../images/cover.jpg'
+    },
+    audioCtx: null
+  },
+
+  onReady: function() {
+    this.audioCtx = wx.createInnerAudioContext()
+    //默认选择第一曲
+    this.setMusic(0)
+  },
+  setMusic: function(index) {
+    var music = this.data.playlist[index]
+    this.audioCtx.src = music.src
+    this.setData({
+      playIndex: index,
+      'play.title': music.title,
+      'play.singer': music.singer,
+      'play.coverImgUrl': music.coverImgUrl,
+      'play.currentTime': '00:00',
+      'play.duration': '00:00',
+      'play.percent': 0
+    })
   },
 
   changeItem: function(e) {
@@ -13,5 +56,6 @@ Page({
     this.setData({
       tab: e.detail.current
     })
-  }
+  },
+  
 })
--- pages/horse/calc/test/test.js
+++ pages/horse/calc/test/test.js
@@ -1,5 +1,28 @@
 Page({
   scroll: function(e) {
     console.log(e.detail)
+  },
+
+  sliderChanging: function(e) {
+    console.log(e.detail.value)
+  }, 
+  
+  onReady: function() {
+    //创建InnerAudioContext实例
+    var audioCtx = wx.createInnerAudioContext()
+    //设置音频资源地址
+    audioCtx.src = 'https://music.163.com/#/song?id=569214247&autoplay=true&market=baiduhd'
+    //当开始播放时，输出调试信息
+    audioCtx.onPlay(function() {
+      console.log('开始播放')
+    })
+    //当发生错误时，输出调试信息
+    audioCtx.onError(function(res) {
+      console.log(res.errMsg)
+      console.log(res.errCode)
+    })
+
+    //开始播放
+    audioCtx.play()
   }
 })
